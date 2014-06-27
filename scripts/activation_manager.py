1f8539a7c86a99539413a25bcb3ab4af1e99a70f
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2014-06-27 16:08:30 +0200

    Changed to actual current limit msg

--- scripts/activation_manager.py
+++ scripts/activation_manager.py
@@ -6,19 +6,17 @@ Node to interact with screen messages to activate
 or deactivate joints to do learning by demonstration
 '''
 import rospy
-from learn_from_robot.msg import JointActivation
+from pal_common_msgs.msg import JointCurrent
 from pal_control_msgs.srv import CurrentLimit, CurrentLimitRequest
 
 ACTIVATION_TOPIC = '/active_joints'
 CURRENT_LIMIT_SRV = '/current_limit_controller/set_current_limit'
 
-ACTIVATED = 1.0
-DEACTIVATED = 0.01
 
 class activation_manager():
     def __init__(self):
         rospy.loginfo("Subscribing to '" + ACTIVATION_TOPIC + "'")
-        self.activation_subs = rospy.Subscriber(ACTIVATION_TOPIC, JointActivation, self.activation_cb)
+        self.activation_subs = rospy.Subscriber(ACTIVATION_TOPIC, JointCurrent, self.activation_cb)
         
         rospy.loginfo("Trying to connect to service'" + CURRENT_LIMIT_SRV + "'...")
         self.current_limit_srv = rospy.ServiceProxy(CURRENT_LIMIT_SRV, CurrentLimit)
@@ -41,7 +39,7 @@ class activation_manager():
         
     def activation_cb(self, data):
         # Check if we got a group or a single joint to activate/deactivate
-        joints = str(data.joints.data)
+        joints = str(data.joints)
         if joints in self.group_names:
             list_of_joints = getattr(self, joints)
         else:
@@ -51,13 +49,12 @@ class activation_manager():
         for joint in list_of_joints:
             req = CurrentLimitRequest()
             req.actuator_name = joint.replace('joint', 'motor')
-            if data.active.data: # If True, activate
-                rospy.loginfo("Setting joint '" + joint + "' to ACTIVE")
-                req.current_limit = ACTIVATED
+            if 0.0 <= data.current_limit <= 1.0:
+                rospy.loginfo("Setting joint '" + joint + "' to " + str(data.current_limit))
+                req.current_limit = data.current_limit
+                self.current_limit_srv.call(req)
             else:
-                rospy.loginfo("Setting joint '" + joint + "' to DEACTIVATED")
-                req.current_limit = DEACTIVATED
-            self.current_limit_srv.call(req)
+                rospy.logwarn("Sent current_limit value: " + str(data.current_limit) + " not in between 0.0 and 1.0")
 
         
         
